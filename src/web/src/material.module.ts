3e474b7cf8c1e451fedb19ec8d79b4286350b7da
Mark Hollomon <mhhollomon@gmail.com>
2022-12-12 12:01:19 -0500

    first web deply

--- src/web/src/material.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
-
-@NgModule({
-  exports: [
-      MatGridListModule
-  ]
-})
-export class MaterialModule {}
--- tests/test1.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-import consolidate
-
-
-class Test1 ( unittest.TestCase) :
-    def my_test(self) :
-        pass
